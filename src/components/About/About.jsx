4a35abd1e61182d5ada56d4a7258d2fb876c901a
unknown <ramenizing@gmail.com>
2024-02-25 16:49:53 +0800

    unfinished about module css

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,23 +1,32 @@
 import React from "react";
 import { getImageUrl } from "../../utils";
 
+import styles from "./About.module.css";
+
 export const About = () => {
     return (
-        <section>
-            <h2>About</h2>
-            <div>
-                <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with a laptop" />
-                <ul>
-                    <li>
+        <section className={styles.container}>
+            <h2 className={styles.title}>About</h2>
+            <div className={styles.content}>
+                <img src={getImageUrl("about/aboutImage.png")} alt="Me sitting with a laptop" className={styles.aboutImage}/>
+                <ul className={styles.aboutItems}>
+                    <li className={styles.aboutItem}>
                         <img src={getImageUrl("about/cursorIcon.png")} alt="Curosr icon" />
-                        <div>
+                        <div className={styles.aboutItemText}>
                             <h3>Front-End Developer</h3>
                             <p>I'm a Front-End Developer with experience in building responsive and optimized sites.</p>
                         </div>
                     </li>
-                    <li>
-                        <img src={getImageUrl("about/cursorIcon.png")} alt="Curosr icon" />
-                        <div>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/serverIcon.png")} alt="Server icon" />
+                        <div className={styles.aboutItemText}>
+                            <h3>Back-End Developer</h3>
+                            <p>I have experience developing fast and optimised backend and API's</p>
+                        </div>
+                    </li>
+                    <li className={styles.aboutItem}>
+                        <img src={getImageUrl("about/cursorIcon.png")} alt="UI icon" />
+                        <div className={styles.aboutItemText}>
                             <h3>Front-End Developer</h3>
                             <p>I'm a Front-End Developer with experience in building responsive and optimized sites.</p>
                         </div>
